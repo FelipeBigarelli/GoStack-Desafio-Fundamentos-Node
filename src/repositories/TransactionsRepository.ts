2d15c36b6667fa1a6a17a54d8a057175bfee0c13
felipebigarelli <felipebigarelli@hotmail.com>
2020-08-17 13:55:30 -0300

    Desafio Fundamentos Node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income'| 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+     const income = this.transactions.reduce((previous, current) => {
+       if (current.type === 'income') {
+         return previous + current.value;
+       };
+       return previous;
+     }, 0);
+
+     const outcome = this.transactions.reduce((previous, current) => {
+       if (current.type === 'outcome') {
+         return previous + current.value;
+       };
+       return previous;
+     }, 0);
+
+     const total = income - outcome;
+
+     return {
+       income,
+       outcome,
+       total,
+     }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: RequestDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    const { total } = this.getBalance();
+
+    if (total < value && type === 'outcome') {
+      throw new Error('Saldo insuficiente!');
+    };
+
+    this.transactions.push(transaction);
+    
+    return transaction;
   }
 }
 
